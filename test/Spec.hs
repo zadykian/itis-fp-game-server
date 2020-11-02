0815fa3ef5c52af578280d9b96937e3653fdc51a
zadykian <zadykian@outlook.com>
2020-11-03 00:01:17 +0300

    unit-тесты

--- test/Spec.hs
+++ test/Spec.hs
@@ -91,65 +91,155 @@ applyTurnToGameState = TestCase (
         (tryApplyTurn currentPlayerTurn gameStateBeforeTurn)
     )
     where
-        currentPlayerTurn = PlayerTurn [toEnum 2, toEnum 3] X
+        currentPlayerTurn = PlayerTurn [toEnum 8, toEnum 3] X
         gameStateBeforeTurn = GameState globalBoardBeforeTurn $ Just $ PlayerTurn [toEnum 5, toEnum 8] O
         gameStateAfterTurn = GameState globalBoardAfterTurn $ Just currentPlayerTurn
 
         {-
             Глобальное поле до применения хода:
             
-            X__ | _OO | O_X
-            _X_ | _X_ | _OX
-            __X | ___ | XXO
-            ---------------
-            OOO | _X_ | _XX
-            X_X | OXO | XOX
-            XXO | _X_ | __O
-            ---------------
-            
+                X__ | _OO | O_X
+                _X_ | _X_ | _OX
+                __X | X__ | _XO
+                ---------------
+                OOO | _X_ | _OX
+                X_X | OXO | XO_
+                XXO | OX_ | __O
+                ---------------
+                XO_ | OOO | __X
+                _O_ | ___ | _OO
+                __X | X_X | O__
         -}
         globalBoardBeforeTurn = GameBoard $ map createLocalBoard
             [
                 -- LocalBoard '0'
-                [ Owned X, Free, Free, Free, Owned X, Free, Free, Free, Owned X ],
+                [
+                    Owned X, Free, Free, 
+                    Free, Owned X, Free, 
+                    Free, Free, Owned X
+                ],
                 -- LocalBoard '1'
-                [ Free, Owned O, Owned O, Free, Owned X, Free, Free, Free, Free ],
+                [
+                    Free, Owned O, Owned O,
+                    Free, Owned X, Free,
+                    Owned X, Free, Free
+                ],
                 -- LocalBoard '2'
-                [ Owned O, Free, Owned X, Free, Owned O, Owned X, Owned X, Owned X, Owned O],
+                [
+                    Owned O, Free, Owned X,
+                    Free, Owned O, Owned X,
+                    Free, Owned X, Owned O
+                ],
                 -- LocalBoard '3'
-                [ Owned O, Owned O, Owned O, Owned X, Free, Owned X, Owned X, Owned X, Owned O],
+                [
+                    Owned O, Owned O, Owned O,
+                    Owned X, Free, Owned X,
+                    Owned X, Owned X, Owned O
+                ],
                 -- LocalBoard '4'
-                -- todo
-                [ Owned X, Free, Free, Free, Owned X, Free, Free, Free, Owned X
-                ],
-                -- локальное поле '5'
                 [
-                    Owned X, Free, Free,
                     Free, Owned X, Free,
+                    Owned O, Owned X, Owned O,
+                    Owned O, Owned X, Free
+                ],
+                -- LocalBoard '5'
+                [
+                    Free, Owned O, Owned X,
+                    Owned X, Owned O, Free,
+                    Free, Free, Owned O
+                ],
+                -- LocalBoard '6'
+                [
+                    Owned X, Owned O, Free,
+                    Free, Owned O, Free,
                     Free, Free, Owned X
                 ],
-                -- локальное поле '6'
+                -- LocalBoard '7'
                 [
                     Owned X, Free, Free,
-                    Free, Owned X, Free,
-                    Free, Free, Owned X
+                    Free, Owned O, Free,
+                    Owned X, Free, Owned X
                 ],
-                -- локальное поле '7'
+                -- LocalBoard '8'
                 [
-                    Owned X, Free, Free,
-                    Free, Owned X, Free,
-                    Free, Free, Owned X
-                ],
-                -- локальное поле '8'
-                [
-                    Owned X, Free, Free,
-                    Free, Owned X, Free,
-                    Free, Free, Owned X
+                    Free, Free, Owned X,
+                    Free, Owned O, Owned O,
+                    Owned O, Free, Free
                 ]
             ]
 
-
-        globalBoardAfterTurn = undefined :: GlobalBoard
+        {-
+            Глобальное поле после применения хода:
+            
+                X__ | _OO | O_X
+                _X_ | _X_ | _OX
+                __X | X__ | _XO
+                ---------------
+                OOO | _X_ | _OX
+                X_X | OXO | XO_
+                XXO | OX_ | __O
+                ---------------
+                XO_ | OOO | __X
+                _O_ | ___ | XOO
+                __X | X_X | O__
+        -}
+        globalBoardAfterTurn = GameBoard $ map createLocalBoard
+            [
+                -- LocalBoard '0'
+                [
+                    Owned X, Free, Free, 
+                    Free, Owned X, Free, 
+                    Free, Free, Owned X
+                ],
+                -- LocalBoard '1'
+                [
+                    Free, Owned O, Owned O,
+                    Free, Owned X, Free,
+                    Owned X, Free, Free
+                ],
+                -- LocalBoard '2'
+                [
+                    Owned O, Free, Owned X,
+                    Free, Owned O, Owned X,
+                    Free, Owned X, Owned O
+                ],
+                -- LocalBoard '3'
+                [
+                    Owned O, Owned O, Owned O,
+                    Owned X, Free, Owned X,
+                    Owned X, Owned X, Owned O
+                ],
+                -- LocalBoard '4'
+                [
+                    Free, Owned X, Free,
+                    Owned O, Owned X, Owned O,
+                    Owned O, Owned X, Free
+                ],
+                -- LocalBoard '5'
+                [
+                    Free, Owned O, Owned X,
+                    Owned X, Owned O, Free,
+                    Free, Free, Owned O
+                ],
+                -- LocalBoard '6'
+                [
+                    Owned X, Owned O, Free,
+                    Free, Owned O, Free,
+                    Free, Free, Owned X
+                ],
+                -- LocalBoard '7'
+                [
+                    Owned X, Free, Free,
+                    Free, Owned O, Free,
+                    Owned X, Free, Owned X
+                ],
+                -- LocalBoard '8'
+                [
+                    Free, Free, Owned X,
+                    Owned X, Owned O, Owned O,
+                    Owned O, Free, Free
+                ]
+            ]
 
         createLocalBoard :: [BoardSegmentState] -> LocalBoard
         createLocalBoard stateList = GameBoard $ map AtomicCell stateList
